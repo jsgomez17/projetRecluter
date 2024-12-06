d52628dda1e119049c2556b9a71386f3b694fdbb
Jenny Gomez <PachonGomez@teccart.online>
2024-12-05 21:32:17 -0500

    Con puerto

--- startup_backend.sh
+++ startup_backend.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
+
 echo "Abriendo backend"
+# Move into the correct directory where the FastAPI app is located
 cd backend
 echo "Iniciando servicio"
-uvicorn app.main:app
+# Run the FastAPI + Dash app using Uvicorn
+uvicorn app.main:app --host 0.0.0.0 --port 8000 --workers 1
 echo "Fin del servicio"
+
+
